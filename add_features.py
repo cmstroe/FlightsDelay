3892b3c17833dcea96a98f4a0925f523878f65ab
DennisHaijma <Dennis.Haijma@hotmail.com>
2020-12-04 19:15:40 +0100

    Making add_features a method and EDA notebook with airport heatmap

--- add_features.py
+++ add_features.py
@@ -1,18 +1,20 @@
-#%%
+def add_features(flights_train):
 
-import pandas as pd
+    """[Adding flights_train objects/features to the flights_train such as airport locations of the origin and destination and datetime]
 
-flights_train = pd.read_csv(r'./data/flights_train.csv')
-airports = pd.read_csv(r'./data/airports.csv')
+    Returns:
+        [DataFrame]: flights_train populated with the additional features
+    """    
 
-###################################################
+    ###################################################
 
-data = pd.merge(flights_train, airports.drop('AIRPORT', axis=1).add_prefix('ORIGIN_'), left_on='ORIGIN_AIRPORT', right_on='ORIGIN_IATA_CODE')
+    flights_train = pd.merge(flights_train, airports.drop('AIRPORT', axis=1).add_prefix('ORIGIN_'), left_on='ORIGIN_AIRPORT', right_on='ORIGIN_IATA_CODE')
 
-data = pd.merge(data, airports.drop('AIRPORT', axis=1).add_prefix('DESTINATION_'), left_on='DESTINATION_AIRPORT', right_on='DESTINATION_IATA_CODE')
+    flights_train = pd.merge(flights_train, airports.drop('AIRPORT', axis=1).add_prefix('DESTINATION_'), left_on='DESTINATION_AIRPORT', right_on='DESTINATION_IATA_CODE')
 
-###################################################
+    ###################################################
 
-data['Datetime'] = pd.to_datetime(data['YEAR'].astype(str) + '-' + data['MONTH'].astype(str) + '-' + data['DAY'].astype(str), format='%Y-%m-%d')
+    flights_train['Datetime'] = pd.to_datetime(flights_train['YEAR'].astype(str) + '-' + flights_train['MONTH'].astype(str) + '-' + flights_train['DAY'].astype(str), format='%Y-%m-%d')
 
+    return flights_train
 # %%
